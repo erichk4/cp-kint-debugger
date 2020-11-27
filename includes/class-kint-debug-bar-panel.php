ec27d41c5075614cdfc93a01541f86643325fbcb
Erich Munz <wuchtl1000@gmx.at>
2020-11-27 21:07:17 +0100

    Update class-kint-debug-bar-panel.php

--- includes/class-kint-debug-bar-panel.php
+++ includes/class-kint-debug-bar-panel.php
@@ -1,32 +1,41 @@
 <?php
-/**
- * Class Kint_Debug_Bar_Panel
- */
-class Kint_Debug_Bar_Panel extends Debug_Bar_Panel {
-	var $_visible = true;
 
-	function title( $title = null ) {
-		return __( 'Kint Debugger active' );
-	}
+    /**
+     * Class Kint_Debug_Bar_Panel
+     */
+    class Kint_Debug_Bar_Panel extends Debug_Bar_Panel
+    {
+        var $_visible = true;
 
-	function prerender() {
-		$this->set_visible( apply_filters( 'kint_debug_display', true ) );
-	}
+        function title( $title = null )
+        {
+            return __( 'Kint Debugger active' );
+        }
 
-	function is_visible() {
-		return $this->_visible;
-	}
+        function prerender()
+        {
+            $this->set_visible( apply_filters( 'kint_debug_display', true ) );
+        }
 
-	function set_visible( $visible ) {
-		$this->_visible = $visible;
-	}
+        function is_visible()
+        {
+            return $this->_visible;
+        }
 
-	function render() {
-		global $kint_debug;
-		if ( is_array( $kint_debug ) ) {
-			foreach ( $kint_debug as $line ) {
-				echo $line;
-			}
-		}
-	}
-}
+        function set_visible( $visible )
+        {
+            $this->_visible = $visible;
+        }
+
+        function render()
+        {
+            global $kint_debug;
+            if ( is_array( $kint_debug ) )
+            {
+                foreach ( $kint_debug as $line )
+                {
+                    echo $line;
+                }
+            }
+        }
+    }
